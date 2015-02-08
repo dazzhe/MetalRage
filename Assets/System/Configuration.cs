14c419b74eb5615f23be864b01c0be8ee0f1cc91
plzw <gigl_1313@yahoo.co.jp>
2015-02-08 21:27:59 +0900

    画質設定を追加
    * ゲーム内設定画面に画質設定用スライダーを追加
    * 関数切り出し、変数名変更などのリファクタリング

--- Assets/System/Configuration.cs
+++ Assets/System/Configuration.cs
@@ -2,50 +2,66 @@
 using System.Collections;
 using UnityEngine.UI;
 
-public class Configuration : MonoBehaviour {
+public class Configuration : MonoBehaviour
+{
 	public static float sensitivity = 2.0f;
 	public static float volume = 0.5f;
-	Canvas _canvas;
-	Slider sensiSL;
-	Text sensiTX;
-	Slider volSL;
-	Text volTX;
+	Canvas canvas;
+	Slider sensitivitySlider;
+	Slider volumeSlider;
+	Slider qualitySlider;
+	Text sensitivityValue;
+	Text volumeValue;
+	Text qualityValue;
+
 	// Use this for initialization
-	void Start () {
-		_canvas = GetComponent<Canvas>();
-		sensiSL = transform.FindChild("Sensitivity").GetComponent<Slider>();
-		sensiTX = transform.FindChild("Sensitivity/SensiText").GetComponent<Text>();
-		volSL = transform.FindChild("Volume").GetComponent<Slider>();
-		volTX = transform.FindChild("Volume/VolumeText").GetComponent<Text>();
+	void Start()
+	{
+		canvas = GetComponent<Canvas>();
+		sensitivitySlider = transform.FindChild("Sensitivity").GetComponent<Slider>();
+		sensitivityValue = transform.FindChild("Sensitivity/Value").GetComponent<Text>();
+		volumeSlider = transform.FindChild("Volume").GetComponent<Slider>();
+		volumeValue = transform.FindChild("Volume/Value").GetComponent<Text>();
+		qualitySlider = transform.FindChild("Quality").GetComponent<Slider>();
+		qualityValue = transform.FindChild("Quality/Value").GetComponent<Text>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		if(Input.GetButtonDown("Menu")){
-			if (!_canvas.enabled)
-				_canvas.enabled = true;
-			else
-				_canvas.enabled = false;
+	void Update()
+	{
+		if (Input.GetButtonDown("Menu")) {
+			ToggleCanvas();
 		}
-		if (_canvas.enabled){
-			if (Screen.lockCursor || !Screen.showCursor){
-				Screen.lockCursor = false;
-				Screen.showCursor = true;
-			}
-			sensitivity = sensiSL.value * 0.2f;
-			sensiTX.text = sensiSL.value.ToString();
-			AudioListener.volume = volSL.value * 0.01f;
-			volTX.text = volSL.value.ToString();
+
+		if (canvas.enabled) {
+			UpdateConfiguration();
 		}
-		/*if (Input.GetButtonDown("sensitivity up")){
-			sensitivity += 0.1f;
-		}
-		if (Input.GetButtonDown("sensitivity down")){
-			sensitivity -= 0.1f;
-		}*/
 	}
 
-	/*void OnGUI(){
-		GUI.Label (new Rect (0, 0, 300, 60),"Sensitivity is "+sensitivity);
-	}*/
+	void ToggleCanvas()
+	{
+		canvas.enabled = !canvas.enabled;
+
+		if (canvas.enabled) {
+			ShowCursor();
+		}
+	}
+	
+	void ShowCursor()
+	{
+		if (Screen.lockCursor || !Screen.showCursor) {
+			Screen.lockCursor = false;
+			Screen.showCursor = true;
+		}
+	}
+
+	void UpdateConfiguration()
+	{
+		sensitivity = sensitivitySlider.value * 0.2f;
+		sensitivityValue.text = sensitivitySlider.value.ToString();
+		AudioListener.volume = volumeSlider.value * 0.01f;
+		volumeValue.text = volumeSlider.value.ToString();
+		QualitySettings.SetQualityLevel((int)qualitySlider.value, true);
+		qualityValue.text = qualitySlider.value.ToString();
+	}
 }
