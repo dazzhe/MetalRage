5a74e4f9802c14da66155b121dfbd089527e4ede
DazzHE <kazsakinotwen@gmail.com>
2015-02-08 13:55:03 +0900

    弾の表示の改善
    遠くに向かって撃ったとき着弾までの表示にかなりラグがあったため
    弾の描画を一回のみにした。

--- Assets/Parts/Weapons/Bullet.cs
+++ Assets/Parts/Weapons/Bullet.cs
@@ -3,24 +3,16 @@ using System.Collections;
 
 public class Bullet : MonoBehaviour {
 	public GameObject impact;
-	private float speed = 10.0F;
 	public Vector3 originPos;
 	public Vector3 targetPos;
-	public Vector3 direction;
-	public int totalRenderingCount;
-	public int renderingCount;
-	// Use this for initialization
+
 	void Start () {
-		direction = targetPos - originPos;
-		totalRenderingCount = Mathf.FloorToInt(direction.magnitude / speed) + 1;
 		StartCoroutine(this.BulletMove());
 	}
 	
 	IEnumerator BulletMove(){
-		for (renderingCount = 0; renderingCount <= totalRenderingCount; renderingCount++){
-			transform.position = originPos + ((float)renderingCount / totalRenderingCount) * direction * speed;
-			yield return new WaitForSeconds(0.02f);
-		}
+		transform.position = Vector3.Lerp(originPos, targetPos, 0.5f + Random.value * 0.2f);
+		yield return new WaitForSeconds(0.07f);
 		GameObject.Instantiate(impact,targetPos,transform.rotation);
 		Destroy (gameObject);
 		//yield return new WaitForSeconds(1f);
