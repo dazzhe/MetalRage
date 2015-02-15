89c5d7fd2c119c8f5ca38d39c0509fbb7386095d
plzw <gigl_1313@yahoo.co.jp>
2015-02-16 00:55:23 +0900

    ゲーム内設定をPlayerPrefsに保存

--- Assets/System/Configuration.cs
+++ Assets/System/Configuration.cs
@@ -4,8 +4,13 @@ using UnityEngine.UI;
 
 public class Configuration : MonoBehaviour
 {
-	public static float sensitivity = 2.0F;
-	public static float volume = 0.5F;
+	public static float sensitivity;
+	public static float volume;
+	public static int quality;
+
+	const string SensitivityKey = "ConfigurationSensitivity";
+	const string SoundVolumeKey = "ConfigurationSoundVolume";
+	const string QualityKey = "ConfigurationQuality";
 
 	[SerializeField] Canvas canvas;
 	[SerializeField] Slider sensitivitySlider;
@@ -19,24 +24,31 @@ public class Configuration : MonoBehaviour
 	{
 		sensitivity = sensitivitySlider.value * 0.2F;
 		sensitivityValue.text = sensitivitySlider.value.ToString();
+		PlayerPrefs.SetFloat(SensitivityKey, sensitivity);
 	}
 	
 	public void UpdateSoundVolume()
 	{
-		AudioListener.volume = volumeSlider.value * 0.01F;
+		volume = volumeSlider.value * 0.01F;
+		AudioListener.volume = volume;
 		volumeValue.text = volumeSlider.value.ToString();
+		PlayerPrefs.SetFloat(SoundVolumeKey, volume);
 	}
 	
 	public void UpdateQuality()
 	{
-		QualitySettings.SetQualityLevel((int)qualitySlider.value, true);
+		quality = (int)qualitySlider.value;
+		QualitySettings.SetQualityLevel(quality, true);
 		qualityValue.text = qualitySlider.value.ToString();
+		PlayerPrefs.SetInt(QualityKey, quality);
 	}
 
 	// Use this for initialization
 	void Start()
 	{
-
+		InitializeSensitivity();
+		InitializeSoundVolume();
+		InitializeQuality();
 	}
 	
 	// Update is called once per frame
@@ -45,6 +57,29 @@ public class Configuration : MonoBehaviour
 		if (Input.GetButtonDown("Menu")) {
 			ToggleCanvas();
 		}
+	}
+	
+	void InitializeSensitivity()
+	{
+		sensitivity = PlayerPrefs.GetFloat(SensitivityKey, 2F);
+		sensitivitySlider.value = sensitivity * 5F;
+		sensitivityValue.text = sensitivitySlider.value.ToString();
+	}
+
+	void InitializeSoundVolume()
+	{
+		volume = PlayerPrefs.GetFloat(SoundVolumeKey, 0.5F);
+		AudioListener.volume = volume;
+		volumeSlider.value = volume * 100F;
+		volumeValue.text = volumeSlider.value.ToString();
+	}
+
+	void InitializeQuality()
+	{
+		quality = PlayerPrefs.GetInt(QualityKey, QualitySettings.GetQualityLevel());
+		QualitySettings.SetQualityLevel(quality, true);
+		qualitySlider.value = (float)quality;
+		qualityValue.text = qualitySlider.value.ToString();
 	}
 
 	void ToggleCanvas()
