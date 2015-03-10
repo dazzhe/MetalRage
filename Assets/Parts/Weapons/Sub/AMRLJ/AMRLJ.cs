61165ae9f7f2e47d72f7fd97b35a02c5dceb8703
DazzHE <kazsakinotwen@gmail.com>
2015-03-10 19:14:31 +0900

    ジャベリンモデル差し替え
    モデルを作成しアニメーションを追加した

--- Assets/Parts/Weapons/Sub/AMRLJ/AMRLJ.cs
+++ Assets/Parts/Weapons/Sub/AMRLJ/AMRLJ.cs
@@ -1,27 +1,30 @@
 using UnityEngine;
 using System.Collections;
 
-public class AMRLJ : Weapon {
-	private Animation _animation;
-	AudioSource repair;
-	Status s;
-	void Awake () {
+public class AMRLJ : Weapon
+{
+	Animation anim;
+	void Awake ()
+	{
 		param.magazine = 4;
 		param.recoilY = 0f;
 		param.minDispersion = 10f;	//only for showing sight
 		param.dispersionGrow = 3f;
 		param.interval = 1.5f;
 		sightPrefabName = "Rocket_Sight";
+		anim = GetComponent<Animation>();
 		Init ();
 	}
 
-	void LateUpdate () {
+	void LateUpdate ()
+	{
 		if(component.wcontrol.inputShot1 && param.load != 0 && !param.cooldown && param.canShot)
 			StartCoroutine("Shot");
 		sight.extent = param.minDispersion * component.wcontrol.desiredDispersion * 2;
 	}
 
-	IEnumerator Shot(){
+	IEnumerator Shot()
+	{
 		param.cooldown = true;
 		for (int i = 0; i <= 1; i++){
 			if (param.load == 0)
@@ -36,16 +39,36 @@ public class AMRLJ : Weapon {
 		param.cooldown = false;
 	}
 
-	void MakeShot(Vector3 targetPos){
+	void MakeShot(Vector3 targetPos)
+	{
 		StartCoroutine("CreateBullet",targetPos);
 	}
 
-	IEnumerator CreateBullet(Vector3 targetPos){
+	IEnumerator CreateBullet(Vector3 targetPos)
+	{
 		Quaternion rot = Quaternion.LookRotation(targetPos - transform.position);
-		GameObject rocket = PhotonNetwork.Instantiate("Rocket",transform.position, rot, 0) as GameObject;
+		GameObject rocket = PhotonNetwork.Instantiate("Rocket",transform.parent.position, rot, 0) as GameObject;
 		Rocket _rocket = rocket.GetComponent<Rocket>();
 		_rocket.shooterPlayer = component.unit.name;
 		_rocket.wcontrol = component.wcontrol;
 		yield return null;
 	}
+
+	protected override void Enable()
+	{
+		anim["SettingUp"].speed = 1;
+		anim.Play("SettingUp");
+		base.Enable();
+	}
+
+	protected override void Disable ()
+	{
+		if (anim["SettingUp"].time == 0){
+			anim["SettingUp"].time = anim["SettingUp"].length;
+		}
+		anim["SettingUp"].speed = -3;
+		anim.Play ("SettingUp");
+		base.Disable ();
+	}
+
 }
