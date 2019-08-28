2a914b3451f1081c0f65c32c2b08a51a475c14e7
Kazumasa Sakiyama <kazsakinotwen@gmail.com>
2019-08-29 01:33:58 +0900

    Fix bug that an unit fall when synchronizing

--- Assets/MetalRage/Scripts/Robots/Synchronizer.cs
+++ Assets/MetalRage/Scripts/Robots/Synchronizer.cs
@@ -14,34 +14,26 @@ public class Synchronizer : Photon.MonoBehaviour, IPunObservable {
 
     private void Update() {
         if (!this.photonView.isMine) {
-            //入力情報の同期だけでは位置のずれが生じるため.
-            //各機体の位置を同期する.
-            //カクツキを防ぐためLerpしている.
-            this.transform.position
-                = Vector3.Lerp(this.transform.position,
-                               this.correctPlayerPos,
-                               Time.deltaTime * 10);
-
+            var isCorrectPosTooFar = Vector3.Distance(this.transform.position, this.correctPlayerPos) > 1f;
+            this.transform.position = isCorrectPosTooFar
+                ? this.correctPlayerPos
+                : Vector3.Lerp(this.transform.position, this.correctPlayerPos, Time.deltaTime * 10f);
             float correctRotationX = ((float)this.correctRotationX + 18000) / 100;
             float correctRotationY = ((float)this.correctRotationY - 120) / 2;
-            //逆回転を防ぐため回転核が360→0や0→360と変化した際Lerp先を修正する.
-            //これでも100→350(0経由)などのように変化した場合逆にLerpしてしまうので修正が必要?.
             if (correctRotationX < 90f && this.motor.rotationX > 270f) {
                 correctRotationX += 360f;
             }
-
             if (correctRotationX > 270f && this.motor.rotationX < 90f) {
                 correctRotationX -= 360f;
             }
-
             this.motor.rotationX
                 = Mathf.Lerp(this.motor.rotationX,
                              correctRotationX,
-                             Time.deltaTime * 20);
+                             Time.deltaTime * 20f);
             this.wcontrol.rotationY
                 = Mathf.Lerp(this.wcontrol.rotationY,
                               correctRotationY,
-                              Time.deltaTime * 20);
+                              Time.deltaTime * 20f);
             if (this.motor.rotationX > 360f) {
                 this.motor.rotationX -= 360f;
             }
@@ -54,15 +46,14 @@ public class Synchronizer : Photon.MonoBehaviour, IPunObservable {
 
     public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info) {
         if (stream.isWriting) {
-            // We own this player: send the others our data
+            // Send data
             stream.SendNext(this.transform.position);
+            // Compress RotationX to 0~360 and RotationY to -60~60.
             stream.SendNext((short)(this.motor.rotationX * 100 - 18000));
             stream.SendNext((byte)(this.wcontrol.rotationY * 2 + 120));
         } else {
-            // Network player, receive data
+            // Receive data
             this.correctPlayerPos = (Vector3)stream.ReceiveNext();
-            //通信量を減らすためサイズを減らす.
-            //RotationXは0~360,RotationYは-60~60の範囲で動く.
             this.correctRotationX = (short)stream.ReceiveNext();
             this.correctRotationY = (byte)stream.ReceiveNext();
         }
